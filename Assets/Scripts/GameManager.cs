a77020977e0f623156c225841a3911f297b51358
Sahil <sahil.kumar5729@gmail.com>
2021-12-21 09:01:22 +0530

    Add movement functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,19 +7,49 @@ public class GameManager : MonoBehaviour
     public GameObject[] zombies;
     public bool isRising = false;
     public bool isFalling = false;
+    public int riseSpeed = 1;
 
+    public Vector2 startPosition;
     private int activeZombieIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        pickNewZombie();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isRising)
+        {
+            if (zombies[activeZombieIndex].transform.position.y - startPosition.y >= 2.5f)
+            {
+                isRising = false;
+                isFalling = true;
+            }
+            else
+            {
+                zombies[activeZombieIndex].transform.Translate(Vector2.up * Time.deltaTime * riseSpeed);
+            }
+        }
+        else if (isFalling)
+        {
+            if (zombies[activeZombieIndex].transform.position.y - startPosition.y <= 0f)
+            {
+                isFalling = false;
+                isRising = false;
+            }
+            else
+            {
+                zombies[activeZombieIndex].transform.Translate(Vector2.down * Time.deltaTime * riseSpeed);
+            }
+        }
+        else
+        {
+            zombies[activeZombieIndex].transform.position = startPosition;
+            pickNewZombie();
+        }
     }
 
     private void pickNewZombie()
@@ -34,7 +64,7 @@ public class GameManager : MonoBehaviour
         GameObject activeZombie = zombies[activeZombieIndex];
         Transform activeTransform = activeZombie.transform;
     
-        Vector2 startPosition = activeTransform.position;
+        startPosition = activeTransform.position;
     }
 
 }
