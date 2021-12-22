4b6e2fb69d493414c2c508f82fa1f86203d180d6
Sahil <sahil.kumar5729@gmail.com>
2021-12-22 12:02:01 +0530

    Add scores and lives

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,50 +9,67 @@ public class GameManager : MonoBehaviour
     public bool isFalling = false;
     public int riseSpeed = 1;
 
+    private int zombiesSmashed;
+    private int livesRemaining;
+    private bool gameOver;
+
     public Vector2 startPosition;
     private int activeZombieIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        pickNewZombie();
+        zombiesSmashed = 0;
+        livesRemaining = 3;
+        gameOver = false;
+        PickNewZombie();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isRising)
+        if (!gameOver)
         {
-            if (zombies[activeZombieIndex].transform.position.y - startPosition.y >= 2.5f)
+
+            if (isRising)
             {
-                isRising = false;
-                isFalling = true;
+                if (zombies[activeZombieIndex].transform.position.y - startPosition.y >= 2.5f)
+                {
+                    isRising = false;
+                    isFalling = true;
+                }
+                else
+                {
+                    zombies[activeZombieIndex].transform.Translate(Vector2.up * Time.deltaTime * riseSpeed);
+                }
+            }
+            else if (isFalling)
+            {
+                if (zombies[activeZombieIndex].transform.position.y - startPosition.y <= 0f)
+                {
+                    isFalling = false;
+                    isRising = false;
+                    livesRemaining--;
+
+                    if (livesRemaining == 0)
+                    {
+                        // game over
+                        gameOver = true;
+                    }
+                }
+                else
+                {
+                    zombies[activeZombieIndex].transform.Translate(Vector2.down * Time.deltaTime * riseSpeed);
+                }
             }
             else
             {
-                zombies[activeZombieIndex].transform.Translate(Vector2.up * Time.deltaTime * riseSpeed);
+                zombies[activeZombieIndex].transform.position = startPosition;
+                PickNewZombie();
             }
-        }
-        else if (isFalling)
-        {
-            if (zombies[activeZombieIndex].transform.position.y - startPosition.y <= 0f)
-            {
-                isFalling = false;
-                isRising = false;
-            }
-            else
-            {
-                zombies[activeZombieIndex].transform.Translate(Vector2.down * Time.deltaTime * riseSpeed);
-            }
-        }
-        else
-        {
-            zombies[activeZombieIndex].transform.position = startPosition;
-            pickNewZombie();
         }
     }
-
-    private void pickNewZombie()
+    private void PickNewZombie()
     {
         isRising = true;
         isFalling = false;
@@ -67,4 +84,14 @@ public class GameManager : MonoBehaviour
         startPosition = activeTransform.position;
     }
 
+    public void KillEnemy()
+    {
+        // increase score
+        zombiesSmashed++;
+
+        // kill enemy
+        zombies[activeZombieIndex].transform.position = startPosition;
+
+        PickNewZombie();
+    }
 }
